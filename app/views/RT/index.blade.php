e7e1df9ea6d6adee8a116107a0af20fc9f4ce84f
calza <diegocalzadi@gmail.com>
2017-06-21 23:06:10 -0300

    Exportar a Excel and Pdf six Photos Informe Seguimiento

--- app/views/RT/index.blade.php
+++ app/views/RT/index.blade.php
@@ -3,16 +3,14 @@
 @section('content')
 <h1 class="sub_header">Informes Técnicos</h1>
 <div class="dataTable_wrapper">
-	<div class="data_table">
-		<table id= "lista-crud" class="table table-striped table-hover table-bordered table-condensed listar-act">
+	<div class="data_table" style="width: 100%;">
+		<table style="width: 57%;" id= "Table_RTech" class="table table-striped table-hover table-bordered table-condensed listar-act">
 			<thead>
 				<tr>
-
+					<th>Exportar Excel</th>
 					<th>Fecha de Envío</th>
 					<th>Enviado por</th>
 					<th>Orden de Mantención</th>
-					<th>Ver</th>
-					<th>Descargar</th>
 					<!--<th>Descripción de la OM</th>
 					<th>Tipo de Equipo</th>
 					<th>Descripción del Equipo</th>
@@ -32,6 +30,7 @@
 				@foreach ($docRTech as $row)
 
 					<tr>
+						<td  class="button_excel"><a href="report_tech/excel/{{$row['Entry']['Id']}}" target="_blank" ><button class="btn btn-block btn-success btn-xs">Excel</button></a></td>
 						<td><?php
 						$startTime = new DateTime($row['Entry']['StartTime']);
 						$startTime->setTimezone(new DateTimeZone('America/Santiago'));
@@ -43,27 +42,9 @@
 							</div>-->
 						</td>
 						<td><?php echo $row['Entry']['UserFirstName']." ".$row['Entry']['UserLastName'];?></td>
-						<td><?php echo $row['Entry']['AnswersJson']['report_technical']['order_manag'];?></td>
-						<td><a href="report_tech/pdf/1/{{$row['Entry']['Id']}}" target="_blank" ><button class="btn btn-block btn-primary btn-xs">Ver</button></a></td>
-						<td><a href="report_tech/pdf/2/{{$row['Entry']['Id']}}" target="_blank" ><button class="btn btn-block btn-success btn-xs">Descargar</button></a></td>
+						<td><?php echo $row['AFAL']['Order_manag'];?></td>
 
-						<!--<td><?php echo $row['Entry']['AnswersJson']['report_technical']['mode_fail'];?>
-						</td>
-						<td><?php echo $row['Entry']['AnswersJson']['report_technical']['equipment'];?></td>
-						<td><?php echo $row['Entry']['AnswersJson']['report_technical']['equipment_desc'];?>
-						</td>
-						<td><?php echo $row['Entry']['AnswersJson']['report_technical']['code'];?>
-						</td>
-						<td><?php echo $row['Entry']['AnswersJson']['report_technical']['date_report_tech'];?>
-						</td>
-						<td><?php echo $row['Entry']['AnswersJson']['report_technical']['report_by'];?>
-						</td>
-						<td><?php echo $row['Entry']['AnswersJson']['report_technical']['company_exec'];?>
-						</td>
-						<td><?php echo $row['Entry']['AnswersJson']['report_technical']['supervisor_plant'];?>
-						</td>
-						<td><?php echo $row['Entry']['AnswersJson']['report_technical']['Loc_technical'];?>
-						</td>-->
+
 
 					</tr>
 					<?php $id++; ?>
